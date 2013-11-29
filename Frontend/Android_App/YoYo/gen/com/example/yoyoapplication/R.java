4e8b4ed6fa5d9764dde1b2a99214d75b0d1deac2
Timsworth <pantalai@hotmail.com>
2013-11-29 18:43:02 +0000

    Created the main app activity

--- Frontend/Android_App/YoYo/gen/com/example/yoyoapplication/R.java
+++ Frontend/Android_App/YoYo/gen/com/example/yoyoapplication/R.java
@@ -24,18 +24,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int btn_discover=0x7f080002;
+        public static final int btn_log_in=0x7f080005;
+        public static final int btn_register=0x7f080004;
+        public static final int ll_bottom_bar=0x7f080003;
+        public static final int ll_search_bar=0x7f080001;
+        public static final int txt_FDF=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int main=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int FDF=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btn_discover=0x7f050004;
+        public static final int btn_log_in=0x7f050006;
+        public static final int btn_register=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int search_hint=0x7f050007;
     }
     public static final class style {
         /** 
