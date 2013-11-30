4c7be3e9dd0fc210ad6e94147b2598691b6e10f1
Mark Leyshon <leysh01@gmail.com>
2013-11-30 04:27:10 +0000

    iOS for the lulz

---
+++ iOS/YoYo Ambition/YoYo Ambition/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YoYo Ambition
+//
+//  Created by Leysh on 29/11/2013.
+//  Copyright (c) 2013 #YoYo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/YoYo Ambition/YoYo Ambition/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YoYo Ambition
+//
+//  Created by Leysh on 29/11/2013.
+//  Copyright (c) 2013 #YoYo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
