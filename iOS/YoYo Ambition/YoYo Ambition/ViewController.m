a63e8dac7850b97dced21ad05d97844a8d163b86
Chris Parsons <christopherkparsons@gmail.com>
2013-11-30 05:51:37 +0000

    refreshing my version of the results page with master

---
+++ iOS/YoYo Ambition/YoYo Ambition/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YoYo Ambition
+//
+//  Created by Leysh on 29/11/2013.
+//  Copyright (c) 2013 #YoYo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
