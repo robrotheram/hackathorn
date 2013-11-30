4ad0e910ddf80067c2e3521991e3418459853a95
Timsworth <pantalai@hotmail.com>
2013-11-30 02:39:25 +0000

    Added register activiy on Android app

--- Frontend/Android_App/YoYo/src/com/example/yoyoapplication/RegisterActivity.java
+++ Frontend/Android_App/YoYo/src/com/example/yoyoapplication/RegisterActivity.java
@@ -1,17 +1,15 @@
 package com.example.yoyoapplication;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 
-public class MainActivity extends Activity {
-
+public class RegisterActivity extends Activity{
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.register);
 	}
 
 	@Override
@@ -19,18 +17,6 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-	
-	public void discoverClick(View view) {
-		startActivity(new Intent("com.example.RESULTS"));
-	}
-	
-	public void registerClick(View view) {
-		startActivity(new Intent("com.example.REGISTER"));
-	}
-	
-	public void loginClick(View view) {
-		
 	}
 	
 }
