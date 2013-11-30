dc6d9a629020dcf70f293628ce2516b0618049b2
Timsworth <pantalai@hotmail.com>
2013-11-30 03:28:29 +0000

    Added buttons to login and register activities.  Forgot to do it previously.

--- Frontend/Android_App/YoYo/src/com/example/yoyoapplication/RegisterActivity.java
+++ Frontend/Android_App/YoYo/src/com/example/yoyoapplication/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.yoyoapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class RegisterActivity extends Activity{
 	
@@ -17,6 +18,10 @@ public class RegisterActivity extends Activity{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void registerClick(View view) {
+		
 	}
 	
 }
